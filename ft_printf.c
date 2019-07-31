762e8ef1afd586c8376d22544362830b147c5ce5
svend <semt1992@gmail.com>
2019-07-31 17:55:53 +0200

    dun

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: stherkil <stherkil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 16:40:39 by stherkil          #+#    #+#             */
-/*   Updated: 2019/07/31 15:59:01 by stherkil         ###   ########.fr       */
+/*   Updated: 2019/07/31 17:55:45 by stherkil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,11 @@ int			getflag(t_data *arginp, char *s)
 		arginp->flagmin = 1;
 	else if (s[0] == '+')
 		arginp->flagplu = 1;
-	else if (s[0] == ' ' && !arginp->flagzer)
-		arginp->flagsp = 1;
+	else if (s[0] == ' ')
+	{
+		if (!arginp->flagzer)
+			arginp->flagsp = 1;
+	}
 	else if (s[0] == '#')
 		arginp->flaghas = 1;
 	else if (s[0] == '0')
@@ -131,7 +134,6 @@ int			getleng(t_data *arginp, char *s)
 void		applydi(va_list valist, t_data *arginp)
 {
 	long long int inp;
-	int len;
 
 	if (arginp->lengno == 1)
 		inp = (long long int)va_arg(valist, int);
@@ -141,7 +143,7 @@ void		applydi(va_list valist, t_data *arginp)
 		inp = va_arg(valist, long long int);
 	else
 		inp = (long long int)va_arg(valist, int);
-	while (arginp->wid > ft_numlen(inp, 10) && arginp->flagmin == 0)
+	while (arginp->wid + arginp->flagplu > ft_numlen(inp, 10) && arginp->flagmin == 0)
 	{
 		if (arginp->flagzer && !arginp->flagsp)
 			ft_putchar('0');
@@ -150,19 +152,63 @@ void		applydi(va_list valist, t_data *arginp)
 		--arginp->wid;
 	}
 	if (inp > 0 && arginp->flagplu == 1)
-	ft_putchar('+');
+		ft_putchar('+');
 	ft_putnbr(inp);
 	while (arginp->wid > ft_numlen(inp, 10) && arginp->flagmin == 1)
 	{
-			ft_putchar(' ');
+		ft_putchar(' ');
 		--arginp->wid;
 	}
 }
 
+void ft_putnbruoxx(unsigned long long int inp, char c)
+{
+	if (c == 'u')
+		ft_putnbr(inp);
+	else if (c == 'x')
+		ft_puthex(inp);
+	else if (c == 'X')
+		ft_puthexu(inp);
+	else if (c == 'o')
+		ft_putoct(inp);
+}
+
 void		applyuoxx(va_list valist, t_data *arginp, char c)
 {
-	if (arginp->flagno == 1)
-		va_arg(valist, int);
+	unsigned long long int inp;
+	int base;
+
+	if (arginp->lengno == 1)
+		inp = (unsigned long long int)va_arg(valist,unsigned int);
+	else if (arginp->lengl == 1)
+		inp = (unsigned long long int)va_arg(valist,unsigned long int);
+	else if (arginp->lengll == 1)
+		inp = va_arg(valist, unsigned long long int);
+	else
+		inp = (unsigned long long int)va_arg(valist, int);
+	if (c == 'u')
+		base = 10;
+	else if (c == 'o')
+		base = 8;
+	else
+		base = 16;
+	while (arginp->wid + arginp->flagplu && inp > ft_numlen(inp, base) && arginp->flagmin == 0)
+	{
+		if (arginp->flagzer && !arginp->flagsp)
+			ft_putchar('0');
+		else
+			ft_putchar(' ');
+		--arginp->wid;
+	}
+	if (arginp->flagplu == 1)
+		ft_putchar('+');
+	if (base == 10)
+	ft_putnbruoxx(inp, base);
+	while (arginp->wid > ft_numlen(inp, base) && arginp->flagmin == 1)
+	{
+		ft_putchar(' ');
+		--arginp->wid;
+	}
 }
 
 void		applyf(va_list valist, t_data *arginp)
@@ -192,7 +238,7 @@ int			getspeci(va_list valist, t_data *arginp, char *s)
 		applyf(valist, arginp);
 	else if (*s == 'c' || *s == 's' || *s == 'p')
 		applycps(valist, arginp, *s);
-	else 
+	else
 		i = 0;
 	return (i);
 }
--- main.c
+++ main.c
@@ -2,17 +2,6 @@
 
 int main(void)
 {
-	ft_printf("b%%%d%dnjour%sbonjour%c\n", (signed char)-20, 10, "bonjour", 'c');
-	/*
-	printf("res is %d\n", ft_printf("ok \n%s%d\n", "et alors\n", 555));
-	printf("res is %d\n", printf("ok \n%s%d\n", "et alors\n", 555));
-	printf ("Characters: %c %c \n", 'a', 65);
-	printf ("Decimals: %d %ld\n", 1977, 650000L);
-	printf ("Preceding with blanks: %10d \n", 1977);
-	printf ("Preceding with zeros: %010d \n", 1977);
-	printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
-	printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
-	printf ("Width trick: %*d \n", 5, 10);
-	printf ("%s \n", "A string");*/
+	ft_printf("bonjour%x\n", 100);
 	return (0);
 }
